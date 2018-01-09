b00df052dc82af4c4eb13ca43b55b949d6c7951d
HyeonmoKim <gusah1224@naver.com>
2018-01-09 15:18:55 +0900

    Modify Line:8, add Line:9

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		Student s = new Student(0025,28,"Mr.Park","3ÇÐ³â");
 		
 		System.out.println(p.age);
-		System.out.println(s.degree);
+		System.out.println(s.pid); //modify) s.degree -> s.pid
+		System.out.println(s.name); // New
 	}
 }
