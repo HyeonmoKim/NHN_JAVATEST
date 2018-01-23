30a93b8983ba8d5077aafba150e8e3919962fb29
jysycw3 <socuez@naver.com>
2018-01-23 18:31:45 +0900

    TESTGIT-1, PPT-4
    이슈생성테스트

---
+++ jyjy/Suber.java
@@ -0,0 +1,11 @@
+public class Suber{
+	public static void mian(String[] args){
+		System.out.println("헬로우 좝아!");
+		System.out.println("헬로우 좝아!");//추가
+		System.out.println("헬로우 좝아!");//추가2
+
+
+		System.out.println("지라깃허브연동테스트");//추가2
+		
+	}
+}
