9ce85041a7749a5d948a29c0d3a9c532ebc4ee5e
jysycw3 <socuez@naver.com>
2018-01-23 17:20:21 +0900

    소스추가

--- jyjy/Yeop.java
+++ jyjy/Yeop.java
@@ -1,5 +1,7 @@
 public class Yeop{
 	public static void mian(String[] args){
 		System.out.println("Çï·Î¿ì ÁÅ¾Æ!");
+		System.out.println("Çï·Î¿ì ÁÅ¾Æ!");//Ãß°¡
+		System.out.println("Çï·Î¿ì ÁÅ¾Æ!");//Ãß°¡2
 	}
 }
